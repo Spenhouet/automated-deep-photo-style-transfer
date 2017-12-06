f4b498faa5b79c0bd365bb7a546e4d064b090b79
Sebastian Penhouet <s.penhouet@outlook.de>
2017-12-06 18:45:35 +0100

    Remove activation save operation since it's no longer needed

--- photo_style_transfer/utils.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-
-def save_layer_activations(conv_layer, filename_format):
-    """Save the layer activations of a given conv layer to the file system"""
-    num_feature_maps = conv_layer.shape[3]
-    for i in range(num_feature_maps):
-        feature_map = conv_layer[0, :, :, i]
-        cv2.imwrite(filename_format % i, feature_map)
