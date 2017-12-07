f64cc5b5d862b224854c2a05e6f518f8e9e5740e
paulsans <p.sanzenbacher@web.de>
2017-12-07 11:26:31 +0100

    Update example for deep photo style transfer

--- photo_style_transfer/example/example.sh
+++ photo_style_transfer/example/example.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python ../style_transfer.py --content_image_path tubingen.jpg --style_image_path seated-nude.jpg --gpu 0,1,2,3
+python3 ../style_transfer.py --content_image_path content.png --style_image_path style.png --gpu 0
