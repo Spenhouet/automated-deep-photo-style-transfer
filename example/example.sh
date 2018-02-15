a667023fd5c545de87e41cafe7dcb99bd9c69142
Sebastian Penhouet <s.penhouet@outlook.de>
2018-02-15 15:11:53 +0100

    Update example

--- example/example.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# clear transfer directory
-rm -rf transfer
-mkdir transfer
-
-# run example
-python ../style_transfer.py --content_image content.png --style_image style.png --content_segmentation content_seg.png --style_segmentation style_seg.png --gpu 0
